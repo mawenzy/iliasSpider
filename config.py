c2f89f0b5f4a536cbbdcacda40a21e0cda8fcb86
mawenzy <mawenzy@users.noreply.github.com>
2020-11-10 15:59:46 +0100

    modified config

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import os
 import platform
 
 class Config():
@@ -8,6 +9,9 @@ class Config():
 		self.ILIAS_URL = ilias_url
 		self.TARGET_DIR = target_dir
 
+def runSpider(c):
+	call = "scrapy crawl ilias -s LOG_ENABLED=False -a username=%s -a iliasUrl=%s -a targetDir=%s"%(c.USERNAME, c.ILIAS_URL, c.TARGET_DIR)
+	os.system(call)
 
 # just a name, is used as id for keyring if you change this after your pwd has been stored 
 # you will need to reenter your password
@@ -18,6 +22,7 @@ PLATFORM = platform.system()
 
 # since urls might change
 ILIAS_LOGIN_URL = 'https://ilias.uni-konstanz.de/ilias/login.php'
+ILIAS_BASE_URL = 'https://ilias.uni-konstanz.de/ilias/'
 
 # only needed for linux
 TMP_DIR = "/tmp/"
---
+++ iliasSpiders.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from config import *
+
+### Spider 1
+c = Config(
+    'vorname.nachname', 
+    'https://ilias.uni-konstanz.de/ilias/goto_ilias_uni_crs_...', 
+    '/path/to/download/folder/', 
+    )
+runSpider(c)
